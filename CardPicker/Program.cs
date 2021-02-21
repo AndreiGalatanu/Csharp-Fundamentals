78a015e5e87a2184266592bb9b0406eca89db71f
Andrei <galatanuandreirafael@gmail.com>
2021-02-21 15:06:10 +0200

    4

---
+++ CardPicker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CardPicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
