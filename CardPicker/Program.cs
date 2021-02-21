462c544d62f9921ac2d39ef45224c90bb9955661
Andrei <galatanuandreirafael@gmail.com>
2021-02-21 15:51:50 +0200

    card done

--- CardPicker/Program.cs
+++ CardPicker/Program.cs
@@ -6,7 +6,25 @@ namespace CardPicker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pick a number");
+
+
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in PickRandom.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(" Please pick a number");
+
+            }
         }
     }
+
+
 }
